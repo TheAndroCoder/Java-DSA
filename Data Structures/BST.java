bc0be416c9af1e9bb0e6886527658c0557e3bbe6
Sachin Rana <35869428+TheAndroCoder@users.noreply.github.com>
2020-08-03 12:43:39 +0530

    Update BST.java

--- Data Structures/BST.java
+++ Data Structures/BST.java
@@ -1,9 +1,11 @@
 package TreeDS;
 
-import java.util.ConcurrentModificationException;
-import java.util.Iterator;
-import java.util.Stack;
+import java.util.*;
 
+/**
+ * Binary Search Tree formation and traversals
+ * @author theandrocoder 
+ */
 public class BSTMain {
     public static void main(String[] args) {
         BST<Integer> bst = new BST<>();
@@ -17,7 +19,21 @@ public class BSTMain {
         Iterator<Integer> it = bst.traverse(BST.TreeTraversalOrder.IN_ORDER);
         while(it.hasNext()){
             System.out.print(it.next()+" ");
-            //it.next();
+        }
+        System.out.println("\nPreorder Traversal");
+        Iterator<Integer> preorder_it = bst.traverse(BST.TreeTraversalOrder.PRE_ORDER);
+        while(preorder_it.hasNext()){
+            System.out.print(preorder_it.next()+" ");
+        }
+        System.out.println("\nPostOrder Traversal");
+        Iterator<Integer> postorder_it = bst.traverse(BST.TreeTraversalOrder.POST_ORDER);
+        while(postorder_it.hasNext()){
+            System.out.print(postorder_it.next()+" ");
+        }
+        System.out.println("\nLevelOrder Traversal");
+        Iterator<Integer> levelorder_it = bst.traverse(BST.TreeTraversalOrder.LEVEL_ORDER);
+        while(levelorder_it.hasNext()){
+            System.out.print(levelorder_it.next()+" ");
         }
     }
 }
@@ -41,10 +57,18 @@ class BST<T extends Comparable<T>>{
         this.size=0;
     }
 
+    /**
+     * returns number of elements in BST at any point of time
+     * @return number of elements
+     */
     public int size(){
         return this.size;
     }
 
+    /**
+     * returns if BST is empty or not
+     * @return true if BST is empty, otherwise returns false
+     */
     public boolean isEmpty(){
         return this.size==0;
     }
@@ -67,6 +91,13 @@ class BST<T extends Comparable<T>>{
         size++;
         return true;
     }
+
+    /**
+     * Recursive version of add method
+     * @param root
+     * @param data
+     * @return
+     */
     private Node<T> add(Node<T> root,T data){
         if(root==null){
             Node<T> node = new Node<>(data);
@@ -82,12 +113,24 @@ class BST<T extends Comparable<T>>{
         return root;
     }
 
+    /**
+     * removes a node from BST
+     * @param data : the value of the node which is to be removed
+     * @return true if node is found and removed, otherwise returns false
+     */
     public boolean remove(T data){
         if(!contains(data))return false;
         root=remove(root,data);
         size--;
         return true;
     }
+
+    /**
+     * Recursive version of remove method
+     * @param root
+     * @param data
+     * @return
+     */
     private Node<T> remove(Node<T> root,T data){
         if(root==null)return null;
         int cmp=data.compareTo(root.data);
@@ -118,9 +161,22 @@ class BST<T extends Comparable<T>>{
         }
         return root;
     }
+
+    /**
+     * checks if the data is present in BST or not
+     * @param data : the data whose presence is to be checked
+     * @return true if the data is found, otherwise returns false
+     */
     private boolean contains(T data) {
         return contains(root,data);
     }
+
+    /**
+     * Recursive version of contains method
+     * @param node
+     * @param data
+     * @return
+     */
     private boolean contains(Node<T> node,T data){
         if(node ==null)return false;
         int cmp=data.compareTo(node.data);
@@ -133,13 +189,25 @@ class BST<T extends Comparable<T>>{
 
     }
 
+    /**
+     * Returns height o0f BST at any point of time
+     * @return : integer : height of tree
+     */
     public int height(){
         return height(root);
     }
+
+    /**
+     * Recursive version of the above method
+     * @param node
+     * @return
+     */
     public int height(Node node){
         if(node==null)return 0;
         return Math.max(height(node.left),height(node.right))+1;
     }
+    
+    //**************** Iterators for tree traversals ********************//
     public enum TreeTraversalOrder{
         PRE_ORDER,IN_ORDER,POST_ORDER,LEVEL_ORDER
     }
@@ -153,72 +221,124 @@ class BST<T extends Comparable<T>>{
         }
     }
 
+    /**
+     * Preorder Traversal of BST
+     * @return Iterator object
+     */
     private Iterator<T> preOrder(){
         int expectedNodeCount=size;
         Stack<Node<T>> st = new Stack<>();
         st.push(root);
         return new Iterator<T>() {
-            Node<T> node=root.left;
+            Node<T> node=root;
+            int i=0;
             @Override
             public boolean hasNext() {
                 if(expectedNodeCount!=size)throw new ConcurrentModificationException();
-                return root!=null && !st.isEmpty();
+                return !st.isEmpty();
             }
 
             @Override
             public T next() {
                 if(expectedNodeCount!=size)throw new ConcurrentModificationException();
-                T data = node.data;
-                while(node !=null){
-                    st.push(node);
-                    node=node.left;
-                }
                 node=st.pop();
-                node=node.right;
+                T data = node.data;
+                if(node.right!=null){
+                    st.push(node.right);
+                }
+                if(node.left!=null){
+                    st.push(node.left);
+                }
                 return data;
 
             }
         };
     }
+
+    /**
+     * InOrder Traversal of BST
+     * @return : Iterator object
+     */
     private Iterator<T> inOrder(){
         int expectedNodeCount=size;
         Stack<Node<T>> st = new Stack<>();
-        st.push(root);
         return new Iterator<T>() {
-            Node<T> node=root.left;
-            boolean bool=true;
+            Node<T> node=root;
             @Override
             public boolean hasNext() {
                 if(expectedNodeCount!=size)throw new ConcurrentModificationException();
-                return root!=null && !st.isEmpty();
+                return node!=null || !st.isEmpty();
             }
 
             @Override
             public T next() {
                 if(expectedNodeCount!=size)throw new ConcurrentModificationException();
-                while(node !=null){
-                    if(bool)
+                while(node!=null){
                     st.push(node);
-                    bool=true;
                     node=node.left;
                 }
                 node=st.pop();
                 T data = node.data;
                 node=node.right;
-                if(node!=null)
-                {
-                    st.push(node);
-                    bool=false;
-                }
                 return data;
 
             }
         };
     }
+
+    /**
+     * PostOrder Traversal of BST
+     * @return : Iterator object
+     */
     private Iterator<T> postOrder(){
-        return null;
+        int expectedNodeCount=size;
+        Stack<Node<T>> st = new Stack<>();
+        st.push(root);
+        Stack<Node<T>> out = new Stack<>();
+        while(!st.empty()){
+            Node<T> curr=st.pop();
+            out.push(curr);
+            if(curr.left!=null)st.push(curr.left);
+            if(curr.right!=null)st.push(curr.right);
+        }
+        return new Iterator<T>() {
+            @Override
+            public boolean hasNext() {
+                if(expectedNodeCount!=size)throw new ConcurrentModificationException();
+                return !out.isEmpty();
+            }
+
+            @Override
+            public T next() {
+                if(expectedNodeCount!=size)throw new ConcurrentModificationException();
+                return out.pop().data;
+
+            }
+        };
     }
+
+    /**
+     * Level Order Traversal of BST
+     * @return : Iterator Object
+     */
     private Iterator<T> levelOrder(){
-        return null;
+        Queue<Node<T>> q=new LinkedList<>();
+        q.add(root);
+        return new Iterator<T>() {
+            Node<T> node=root;
+            @Override
+            public boolean hasNext() {
+                return !q.isEmpty();
+            }
+
+            @Override
+            public T next() {
+                node=q.poll();
+                T data=node.data;
+                if(node.left!=null)q.add(node.left);
+                if(node.right!=null)q.add(node.right);
+                return data;
+            }
+        };
     }
 }
